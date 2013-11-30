6b5c4e18c2b25f84a6ac6ab48a365677bbd98d6c
Chris Lamberson <clamberson@gmail.com>
2013-11-29 20:18:04 -0800

    beginning documentation

--- DUI/dui.h
+++ DUI/dui.h
@@ -6,13 +6,24 @@
 # define DUI_API __declspec(dllimport)
 #endif
 
+/**
+ * \brief The string that represents the DirectUI window class name.
+ */
 #ifdef UNICODE
 # define WC_DIRECTUI L"DirectUI"
 #else
 # define WC_DIRECTUI "DirectUI"
 #endif
 
+/**
+ * \brief Notification sent to the parent window when Direct2D resources need
+ *        to be recreated.
+ */
 #define DUIN_RECREATE 0x1
+
+/**
+ * \brief Notification sent to the parent window when drawing should occur.
+ */
 #define DUIN_DRAW     0x2
 
 typedef struct _DUIHDR {
@@ -20,5 +31,16 @@ typedef struct _DUIHDR {
     ID2D1DCRenderTarget *d2dTarget;
 } DUIHDR, *PDUIHDR;
 
+/**
+ * \brief Registers the DirectUI control and initializes any resources
+ *        associated with the library.
+ * \sa DUI_Uninitialize()
+ */
 EXTERN_C DUI_API void DUI_Initialize(void);
+
+/**
+ * \brief Unregisters the DirectUI control and cleans up any resources
+ *        associated with the library
+ * \sa DUI_Initialize()
+ */
 EXTERN_C DUI_API void DUI_Uninitialize(void);
