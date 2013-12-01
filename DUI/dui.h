7228f055353f93cac8e7e3bec5037a1a3fdb47eb
Chris Lamberson <clamberson@gmail.com>
2013-11-30 18:33:30 -0800

    basic documentation finished

--- DUI/dui.h
+++ DUI/dui.h
@@ -1,3 +1,8 @@
+/**
+ * @file dui.h
+ * @brief The main DirectUI library header.
+ */
+
 #pragma once
 
 #ifdef DUI_EXPORTS
@@ -7,7 +12,8 @@
 #endif
 
 /**
- * \brief The string that represents the DirectUI window class name.
+ * The DirectUI window class. Use this value for any calls to CreateWindow and
+ * friends.
  */
 #ifdef UNICODE
 # define WC_DIRECTUI L"DirectUI"
@@ -16,31 +22,39 @@
 #endif
 
 /**
- * \brief Notification sent to the parent window when Direct2D resources need
- *        to be recreated.
+ * Notification to recreate Direct2D resources. This notification is sent to
+ * the parent window when Direct2D resources have become dirty for any reason
+ * the API deems necessary. When received, recreate any device-dependent
+ * resources needed by your program.
  */
 #define DUIN_RECREATE 0x1
 
 /**
- * \brief Notification sent to the parent window when drawing should occur.
+ * Notification to perform any drawing. This notification is sent to the parent
+ * window whenever the DirectUI is asked to paint itself.
  */
 #define DUIN_DRAW     0x2
 
+/**
+ * Custom NMHDR sent by DirectUI. All notification messages sent from a
+ * DirectUI control to its parent window send this header as the lParam value.
+ */
 typedef struct _DUIHDR {
-    NMHDR hdr;
-    ID2D1DCRenderTarget *d2dTarget;
-} DUIHDR, *PDUIHDR;
+  NMHDR hdr;                      /**< Parent NMHDR */
+  ID2D1DCRenderTarget *d2dTarget; /**< Render target for the control's DC  */
+} DUIHDR;
+typedef DUIHDR *PDUIHDR;
 
 /**
- * \brief Registers the DirectUI control and initializes any resources
- *        associated with the library.
- * \sa DUI_Uninitialize()
+ * Initializes the DirectUI library. Registers the DirectUI control and
+ * allocates any resources associated with the library.
+ * @see DUI_Uninitialize()
  */
 EXTERN_C DUI_API void DUI_Initialize(void);
 
 /**
- * \brief Unregisters the DirectUI control and cleans up any resources
- *        associated with the library
- * \sa DUI_Initialize()
+ * Tear down the DirectUI library. Unregisters the DirectUI control and cleans
+ * up any resources associated with the library.
+ * @see DUI_Initialize()
  */
 EXTERN_C DUI_API void DUI_Uninitialize(void);
